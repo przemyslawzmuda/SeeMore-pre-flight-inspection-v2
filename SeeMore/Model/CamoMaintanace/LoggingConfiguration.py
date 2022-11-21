b9b2c5b8e63ed8a358a69478c7b0b203dcbc732f
przemyslawzmuda <88719600+przemyslawzmuda@users.noreply.github.com>
2022-11-21 20:31:00 +0100

    Add Google docstring.

--- SeeMore/Model/CamoMaintanace/LoggingConfiguration.py
+++ SeeMore/Model/CamoMaintanace/LoggingConfiguration.py
@@ -10,8 +10,16 @@ class SeeMoreLoggingFacility:
 
     @staticmethod
     def return_name_for_logger_object(file_name: str) -> str:
+        """The following function returns the name for a logger object in a given script.
+        Args:
+            file_name (str): A root path to the following script.
+            For instance: /Home/User/ProjectDirectory/SeeMore/App/ScriptName.py
+        Returns:
+             str: Name for a logger object for a given script.
+        """
         # type(__file__) = <class 'str'>
         file_name_parts_list = Path(file_name).parts
+        # In order to hide the Home directory of the user, perform the following logic:
         index_for_project_name = file_name_parts_list.index('SeeMore')
         start_index_for_logger_name = len(file_name_parts_list) - index_for_project_name
         name_for_logger_object = str(Path('/')).join(file_name_parts_list[-start_index_for_logger_name:])
