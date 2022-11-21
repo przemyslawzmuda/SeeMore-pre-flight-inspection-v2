40b61119315c12a1343a8b6da350084eeea9a61b
przemyslawzmuda <88719600+przemyslawzmuda@users.noreply.github.com>
2022-11-21 20:17:17 +0100

    Add static method to return name for logger object.

--- SeeMore/Model/CamoMaintanace/LoggingConfiguration.py
+++ SeeMore/Model/CamoMaintanace/LoggingConfiguration.py
@@ -1,6 +1,18 @@
+from pathlib import Path
+
+
 class SeeMoreLoggingFacility:
     """The SeeMoreLoggingFacility gives everything what is necessary to report and record progress as well as
     problems, moreover the destiny is to help developer track down bugs and leave a record how the code is being used.
     The ultimate goal of that class is to improve reliable of the code.
     Available levels for loggers object: NOTSET: 0, DEBUG: 10, INFO: 20, WARNING: 30, ERROR: 40, CRITICAL: 50.
     """
+
+    @staticmethod
+    def return_name_for_logger_object(file_name: str) -> str:
+        # type(__file__) = <class 'str'>
+        file_name_parts_list = Path(file_name).parts
+        index_for_project_name = file_name_parts_list.index('SeeMore')
+        start_index_for_logger_name = len(file_name_parts_list) - index_for_project_name
+        name_for_logger_object = str(Path('/')).join(file_name_parts_list[-start_index_for_logger_name:])
+        return name_for_logger_object
