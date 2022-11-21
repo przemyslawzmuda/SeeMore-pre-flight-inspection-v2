e54ecfe9ef9444907e4d6656da91bc7f893ca990
przemyslawzmuda <88719600+przemyslawzmuda@users.noreply.github.com>
2022-11-21 19:51:50 +0100

    Add Google doc for SeeMoreLoggingFacility.

---
+++ SeeMore/Model/CamoMaintanace/LoggingConfiguration.py
@@ -0,0 +1,6 @@
+class SeeMoreLoggingFacility:
+    """The SeeMoreLoggingFacility gives everything what is necessary to report and record progress as well as
+    problems, moreover the destiny is to help developer track down bugs and leave a record how the code is being used.
+    The ultimate goal of that class is to improve reliable of the code.
+    Available levels for loggers object: NOTSET: 0, DEBUG: 10, INFO: 20, WARNING: 30, ERROR: 40, CRITICAL: 50.
+    """
